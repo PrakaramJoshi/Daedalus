fb111a33546eb0b6354838335be12d53d8069ec9
prakaramjoshi <prakaramjoshi@ubuntu.ubuntu-domain>
2012-09-26 01:58:29 -0400

    Daedalus comes to life!

---
+++ Ganesh/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: prakaramjoshi
+ *
+ * Created on September 26, 2012, 1:57 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
